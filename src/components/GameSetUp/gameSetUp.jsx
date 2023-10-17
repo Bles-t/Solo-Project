77824b444f61fd6ca288a072c2cc8cdcf86f0335
Adrian Bryant <bryant.adrian94@gmail.com>
2023-10-17 14:40:38 -0500

    added player 1 reducer

--- src/components/GameSetUp/gameSetUp.jsx
+++ src/components/GameSetUp/gameSetUp.jsx
@@ -1,16 +1,26 @@
 import React from "react"
-
+import { useDispatch } from "react-redux";
 
 
 function GameSetUp() {
 
+const dispatch = useDispatch()
+
+
+
+  return (
+
+<form onSubmit={handleSubmit}>
+<h2>Enter Match Details</h2>
+
+<input name="name" placeholder="Enter Here" type="text" value={newComments} onChange={event => setNewComments(event.target.value)} />
+
+<button type='submit'> Submit</button>
+
+</form>
+
+    )
 }
-
-
-
-
-
-
 
 
 
---
+++ src/redux/reducers/player1.reducer.js
@@ -0,0 +1,10 @@
+const PlayerOne = (state = [], action) => {
+  if (action.type === 'ADD_PLAYERONE') {
+      console.log(`The element was ${action.payload}`);
+
+      return action.payload
+  }
+  return state
+}
+
+export default PlayerOne;
