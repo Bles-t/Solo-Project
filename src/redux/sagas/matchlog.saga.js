83bbca9a915705e4f4bf8073679bfc3debee0391
Adrian Bryant <bryant.adrian94@gmail.com>
2023-10-26 19:18:52 -0500

    all win data is working exect getting error when trying to use it twice. going to work on history page

--- src/redux/sagas/matchlog.saga.js
+++ src/redux/sagas/matchlog.saga.js
@@ -26,12 +26,35 @@ function* newMatchLog(action) {
 
 
 
+function* newMatchLog2(action) {
+  try {
+    // const logData = { winner: action.payload };
+    // // const logMatch = {
+    // //   winner: winner,
+    // // }
+    const { playerOne, playerTwo, gameTitle, userId } = action.payload;
+    const logData = {
+      winner: playerTwo,
+      loser: playerOne,
+      gametitle: gameTitle,
+      date: new Date(),
+      userId
+    }
+    yield axios.post('/matches', logData);
+    // yield put({ type: 'GET_GAME', win: newmatchlog });
+    console.log('Game title successfully added to the database.');
+  } catch (error) {
+    console.log('error posting an gametitle', error);
+  }
+}
+
+
 
 
 function* matchLogSaga() { //also known as watcherSaga
   yield all([
     takeEvery('PLAYERONEWIN', newMatchLog),
-    takeEvery('WINBUTTON', newMatchLog),
+    takeEvery('WINBUTTON', newMatchLog2),
 
   ])
 }
