37f9a53256d3a9fbe8b64e62787e225aac405e0e
Adrian Bryant <bryant.adrian94@gmail.com>
2023-11-08 20:15:34 -0600

    Alpha v.1

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import ColorfulText from '../../ColorfulText';
 // This is one of our simplest components
 // It doesn't have local state
 // It doesn't dispatch any redux actions or display any part of redux state
@@ -8,7 +8,44 @@ import React from 'react';
 function InfoPage() {
   return (
     <div className="container">
-      <p>Info Page</p>
+
+
+<br />
+<br />
+<br />
+      <br />
+      <p><ColorfulText   text={"Info Page"}   ></ColorfulText></p>
+<p>
+<ul className='font'  >
+  <p>  <ColorfulText text={" Technologies Used: "}></ColorfulText>   </p>
+  <li>
+ <ColorfulText text={"React"}></ColorfulText>
+  <li>
+  <ColorfulText text={"  Redux"}></ColorfulText>
+  </li>
+  <li>
+  <ColorfulText text={"    Node.JS"}></ColorfulText>
+  </li>
+  <li>
+  <ColorfulText text={" Express"}></ColorfulText>
+  </li>
+  <li>
+  <ColorfulText text={"    Axios"}></ColorfulText>
+  </li>
+  <li>
+  <ColorfulText text={"    Javascript"}></ColorfulText>
+  </li>
+  <li>
+  <ColorfulText text={"         Bootstrap"}></ColorfulText>
+  </li>
+  <li>
+
+  </li>
+  </li>
+</ul>
+  <ColorfulText   text={"React ,Redux ,Node.Js, Express ,Axios ,Javascript  and Bootstrap "}   ></ColorfulText>
+   </p>
+
     </div>
   );
 }
